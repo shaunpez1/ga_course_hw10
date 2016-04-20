b37ca0424416ab8a134efac1538906e5019383d8
Shaun Pezeshki <shaunpez@gmail.com>
2016-04-19 19:06:59 -0700

    Added a conditional to handle if the user is not logged in

--- js/app.js
+++ js/app.js
@@ -16,47 +16,51 @@ $(document).ready(function(){
   }
 
   $(document).on("click", ".find-me", function(e){
-    e.preventDefault();
+    if(window.sessionStorage.getItem("instagram_access_token")){
+      e.preventDefault();
 
-    if (!navigator.geolocation){
-      alert("Geolocation is not supported by your browser");
+      if (!navigator.geolocation){
+        alert("Geolocation is not supported by your browser");
 
-    }else if(window.sessionStorage.getItem("instagram_lat") && window.sessionStorage.getItem("instagram_lng")){
+      }else if(window.sessionStorage.getItem("instagram_lat") && window.sessionStorage.getItem("instagram_lng")){
 
-    }else{
-      navigator.geolocation.getCurrentPosition(success, error);
-    }
-
-    function success(position) {
-      var latitude  = position.coords.latitude;
-      var longitude = position.coords.longitude;
-      window
-        .sessionStorage
-        .setItem("instagram_lat", latitude);
-      window
-        .sessionStorage
-        .setItem("instagram_lng", longitude);
-    }
-
-    function error() {
-      alert("Couldn't get your location");
-    }
-
-    $.ajax({
-      url : "https://api.instagram.com/v1/media/search?lat="+ window.sessionStorage.getItem("instagram_lat") + "&lng=" + window.sessionStorage.getItem("instagram_lng") + "&distance=5000&access_token=" + window.sessionStorage.getItem("instagram_access_token", accessToken),
-      dataType: "jsonp",
-      type : "GET",
-
-      success : function(images){
-        images.data.forEach(function(image){
-          $("#image-container").append(imageTemplate(image.images.standard_resolution));
-        });
-      },
-      error : function(){
-        alert("Could not get photos near you");
+      }else{
+        navigator.geolocation.getCurrentPosition(success, error);
       }
 
-    });
+      function success(position) {
+        var latitude  = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        window
+        .sessionStorage
+        .setItem("instagram_lat", latitude);
+        window
+        .sessionStorage
+        .setItem("instagram_lng", longitude);
+      }
+
+      function error() {
+        alert("Couldn't get your location");
+      }
+
+      $.ajax({
+        url : "https://api.instagram.com/v1/media/search?lat="+ window.sessionStorage.getItem("instagram_lat") + "&lng=" + window.sessionStorage.getItem("instagram_lng") + "&distance=5000&access_token=" + window.sessionStorage.getItem("instagram_access_token", accessToken),
+        dataType: "jsonp",
+        type : "GET",
+
+        success : function(images){
+          images.data.forEach(function(image){
+            $("#image-container").append(imageTemplate(image.images.standard_resolution));
+          });
+        },
+        error : function(){
+          alert("Could not get photos near you");
+        }
+
+      });
+    }else{
+      alert("Please login to Instagram");
+    }
 
   });
 
